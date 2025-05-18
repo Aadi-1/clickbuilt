1dcb87e9c89e4759368a8298c4bb8ae1ae570702
Aadi <63931334+Aadi-1@users.noreply.github.com>
2025-05-18 14:42:23 -0700

    fixed hopefully

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,18 +41,18 @@ export default function RootLayout({
             name="ClickBuilt Digital Consulting"
             content="ClickBuilt Digital Consulting Providing Website Design, SEO, and Digital Ads Management to Help Businesses Grow With Proven Lead Generation Strategies."
           />
-          <Script
-            id="gtm-head"
-            strategy="beforeInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        </Head>
+        <Script
+          id="gtm-head"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-KNBDPQ8X');`,
-            }}
-          />
-        </Head>
+          }}
+        />
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-NF2LXM7Q"
